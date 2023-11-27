43dc9f76662a18369357668d1af509006331668b
Shisir Humagain <Shisirhumagain0@gmail.com>
2023-11-27 08:49:21 -0500

    files added

---
+++ src/main/java/org/example/concurrency/virtualThread/MultipleVirtualThreads.java
@@ -0,0 +1,34 @@
+package org.example.concurrency.virtualThread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleVirtualThreads {
+
+    public static void main(String[] args) {
+        List<Thread> vThreads = new ArrayList<>();
+
+        int vThreadCount = 100_000;
+
+        for(int i=0; i<vThreadCount; i++){
+            int vThreadIndex = 1;
+            Thread vThread = Thread.ofVirtual().start(() -> {
+                int result = 1;
+                for(int j=0; j< 10; j++){
+                    result *= (j + 1);
+                }
+                System.out.println("Result[" + vThreadIndex + "]: " + result);
+            });
+
+            vThreads.add(vThread);
+        }
+
+        for(int i=0; i<vThreads.size();i++){
+            try{
+                vThreads.get(i).join();
+            } catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/concurrency/virtualThread/VirtualThreadExample.java
@@ -0,0 +1,28 @@
+package org.example.concurrency.virtualThread;
+
+
+public class VirtualThreadExample {
+
+    public static void main(String[] args) {
+        //Example 1: create Runnable. Create and start virtual thread
+        Runnable runnable = () -> {
+            for(int i =0; i < 10; i++){
+                System.out.println("Index: " + i);
+            }
+        };
+
+        Thread vThread1 = Thread.ofVirtual().start(runnable);
+
+        //Example 2: Create but do not start virtual thread
+        Thread vThreadUnstarted = Thread.ofVirtual().unstarted(runnable);
+
+        vThreadUnstarted.start();
+
+        //Example 4: How to join a virtual thread
+        try {
+            vThreadUnstarted.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
